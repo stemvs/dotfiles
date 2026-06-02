de6507a98f780582bc98057a78ba9902ba74e433
stemvs <browser2@protonmail.com>
2026-06-01 21:23:39 -0600

    sync

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,8 @@
 function config
     /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
 end
+function csync
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add -u
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit -m "sync"
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME push
+end
