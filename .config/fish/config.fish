07ec4ef247a8145a6f05ab395dba1fa3284024b7
stemvs <browser2@protonmail.com>
2026-06-01 21:22:18 -0600

    sync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 set fish_greeting
 
-abbr config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+#abbr config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 #abbr csync '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add -u && /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit -m "sync" && /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME push'
 end
