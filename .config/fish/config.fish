ac2adbba75f90ff157326d02c82ae8a03945551a
stemvs <browser2@protonmail.com>
2026-06-30 11:45:03 -0600

    sync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,10 @@ set fish_greeting
 #abbr config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 #abbr csync '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add -u && /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME commit -m "sync" && /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME push'
 end
+
+# Added by LM Studio CLI tool (lms)
+set -gx PATH $PATH /home/browser/.lmstudio/bin
+
+set -gx PATH "$HOME/.local/bin:$PATH"
+
+set -gx SEARXNG_URL "http://127.0.0.1:8080"
